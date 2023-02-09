4116658e16074f527506c5ae8318e8c6f68fb12d
Tadeo Barranco <tbarrancoa@gmail.com>
2023-02-09 08:02:25 -0600

    [Contact] - Saving Data From Contact Form

--- Plugin/SaveFormData.php
+++ Plugin/SaveFormData.php
@@ -4,20 +4,33 @@ namespace Barranco\Contact\Plugin;
 
 use Psr\Log\LoggerInterface;
 use Magento\Contact\Model\MailInterface;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Barranco\Contact\Api\ContactRepositoryInterface;
+use Barranco\Contact\Model\ContactFactory;
 
 class SaveFormData
 {
     private LoggerInterface $logger;
 
+    private ContactFactory $contactFactory;
+
+    private ContactRepositoryInterface $contactRepository;
+
     /**
      * Class constructor
      *
      * @param \Psr\Log\LoggerInterface $logger
+     * @param \Barranco\Contact\Model\ContactFactory $contactFactory
+     * @param \Barranco\Contact\Api\ContactRepositoryInterface $contactRepository
      */
     public function __construct(
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        ContactFactory $contactFactory,
+        ContactRepositoryInterface $contactRepository
     ) {
         $this->logger = $logger;
+        $this->contactFactory = $contactFactory;
+        $this->contactRepository = $contactRepository;
     }
 
     /**
@@ -30,6 +43,26 @@ class SaveFormData
     public function beforeSend(MailInterface $subject, $replyTo, array $variables)
     {
         $this->logger->info('Save contact form before send email');
+
+        $data = $variables['data']->getData();
+
+        if (isset($data['telephone'])) {
+            $data['phone'] = $data['telephone'];
+            unset($data['telephone']);
+        }
+
+        $this->logger->info(print_r($data, true));
+
+        $contact = $this->contactFactory->create();
+        $contact->setData($data);
+
+        try {
+            $this->contactRepository->save($contact);
+        } catch (\Exception $e) {
+            throw new CouldNotSaveException(
+                __('The contact data is invalid. Verify the data and try again.')
+            );
+        }
 
         return [$replyTo, $variables];
     }
