116907209a763abd7a1f17c15b7ba1449cd034d8
Tadeo Barranco <tbarrancoa@gmail.com>
2023-04-05 09:28:14 -0500

    [Contact] - Get And Validate Contact Inbox In The Admin Controller

--- Controller/Adminhtml/Inbox/View.php
+++ Controller/Adminhtml/Inbox/View.php
@@ -2,10 +2,15 @@
 
 namespace Barranco\Contact\Controller\Adminhtml\Inbox;
 
+use Barranco\Contact\Api\ContactRepositoryInterface;
+use Barranco\Contact\Api\Data\ContactInterface;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
+use Magento\Backend\Model\View\Result\Page;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Result\PageFactory;
 
 class View extends Action implements HttpGetActionInterface
@@ -14,27 +19,67 @@ class View extends Action implements HttpGetActionInterface
 
     private PageFactory $pageFactory;
 
+    private ContactRepositoryInterface $contactRepository;
+
+    private RedirectFactory $redirectFactory;
+
     /**
      * Class constructor
      *
      * @param Context $context
      * @param PageFactory $pageFactory
+     * @param ContactRepositoryInterface $contactRepository
      */
     public function __construct(
         Context $context,
-        PageFactory $pageFactory
+        PageFactory $pageFactory,
+        ContactRepositoryInterface $contactRepository,
+        RedirectFactory $redirectFactory
     ) {
         parent::__construct($context);
         $this->pageFactory = $pageFactory;
+        $this->contactRepository = $contactRepository;
+        $this->redirectFactory = $redirectFactory;
     }
 
     /**
      * Inbox information page
      *
      * @return ResultInterface
+     * @throws NoSuchEntityException
      */
     public function execute(): ResultInterface
     {
-        return $this->pageFactory->create();
+        $inbox = $this->getInbox();
+
+        if (!$inbox) {
+            $redirect = $this->redirectFactory->create();
+            $redirect->setPath('contact/index');
+            return $redirect;
+        }
+
+        /** @var Page $page */
+        $page = $this->pageFactory->create();
+        $page->setActiveMenu('Barranco_Contact::manage');
+        $page->getConfig()->getTitle()->prepend(sprintf('Contact Inbox #%s', $inbox->getId()));
+
+        return $page;
+    }
+
+    /**
+     * Get contact inbox using id from request param
+     *
+     * @return ContactInterface|null
+     */
+    private function getInbox(): ?ContactInterface
+    {
+        try {
+            $inbox = $this->contactRepository->getById($this->getRequest()->getParam('id'));
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage(__($e->getMessage()));
+            return null;
+        }
+
+        return $inbox;
     }
 }
