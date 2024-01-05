3418efc8a5bd92b6b1f943d84f790fe575c3f52a
Tadeo Barranco <tbarrancoa@gmail.com>
2024-01-05 07:28:41 -0600

    [Contact] - Progress Bar Ui Component

---
+++ view/frontend/web/js/view/progress-bar.js
@@ -0,0 +1,19 @@
+/**
+ * Barranco Contact
+ */
+
+define([
+    'uiComponent'
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Barranco_Contact/progress-bar'
+        },
+
+        initialize: function () {
+            this._super();
+        }
+    });
+});
