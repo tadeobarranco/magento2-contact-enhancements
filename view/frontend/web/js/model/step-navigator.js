461002ca64a7bcaa087f60fb2cbdf8bd59e25812
Tadeo Barranco <tbarrancoa@gmail.com>
2024-01-09 07:59:35 -0600

    [Contact] - Add The Ability To Register Step

--- view/frontend/web/js/model/step-navigator.js
+++ view/frontend/web/js/model/step-navigator.js
@@ -10,6 +10,23 @@ define([
     let steps = ko.observableArray();
 
     return {
-        steps: steps
+        steps: steps,
+
+        /**
+         * Push data to the steps array
+         *
+         * @param code
+         * @param title
+         * @param isVisible
+         * @param sortOrder
+         */
+        registerStep: function (code, title, isVisible, sortOrder) {
+            steps.push({
+               code: code,
+               title: title,
+               isVisible: isVisible,
+               sortOrder: sortOrder
+            });
+        }
     }
 });
