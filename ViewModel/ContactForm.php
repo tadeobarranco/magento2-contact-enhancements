fbd347e49b26d980a13a501d0ee86452b56e0135
Tadeo Barranco <tbarrancoa@gmail.com>
2024-05-28 09:03:28 -0500

    [Contact] - Defining A ViewModel

---
+++ ViewModel/ContactForm.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Barranco\Contact\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class ContactForm implements ArgumentInterface
+{
+
+}
