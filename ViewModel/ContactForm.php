f497e79942ad5139fa0c8d6d918ab70f15260cdd
Tadeo Barranco <tbarrancoa@gmail.com>
2024-05-29 08:51:27 -0500

    [Contact] - Initialize window.contactConfig

--- ViewModel/ContactForm.php
+++ ViewModel/ContactForm.php
@@ -2,9 +2,47 @@
 
 namespace Barranco\Contact\ViewModel;
 
+use Barranco\Contact\Model\CompositeConfigProvider;
+use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class ContactForm implements ArgumentInterface
 {
+    private CompositeConfigProvider $configProvider;
 
+    private SerializerInterface $serializer;
+
+    /**
+     * Class constructor
+     *
+     * @param CompositeConfigProvider $configProvider
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(
+        CompositeConfigProvider $configProvider,
+        SerializerInterface $serializer
+    ) {
+        $this->configProvider = $configProvider;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Get configuration
+     *
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        return $this->configProvider->getConfig();
+    }
+
+    /**
+     * Get serialized configuration
+     *
+     * @return bool|string
+     */
+    public function getSerializedConfig(): bool|string
+    {
+        return $this->serializer->serialize($this->getConfig());
+    }
 }
