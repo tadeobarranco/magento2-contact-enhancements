14db79361be9995e56d627a468b6cb2aa6bdfdb7
Tadeo Barranco <tbarrancoa@gmail.com>
2024-05-30 08:15:03 -0500

    [Contact] - Add storeCode To The Default Config Provider Class

--- Model/DefaultConfigProvider.php
+++ Model/DefaultConfigProvider.php
@@ -2,13 +2,43 @@
 
 namespace Barranco\Contact\Model;
 
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
+
 class DefaultConfigProvider implements ConfigProviderInterface
 {
+    private StoreManagerInterface $storeManager;
+
+    /**
+     * Class constructor
+     *
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        StoreManagerInterface $storeManager
+    ) {
+        $this->storeManager = $storeManager;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function getConfig(): array
     {
-        return [];
+        $config = [];
+        $config['storeCode'] = $this->getStoreCode();
+
+        return $config;
+    }
+
+    /**
+     * Get store code
+     *
+     * @return string
+     * @throws NoSuchEntityException
+     */
+    private function getStoreCode(): string
+    {
+        return $this->storeManager->getStore()->getCode();
     }
 }
