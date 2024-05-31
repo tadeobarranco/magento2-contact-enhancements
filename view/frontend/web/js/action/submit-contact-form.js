b7319f9f7f2ef3cf19fa7f64c5a7c4d6a21c7104
Tadeo Barranco <tbarrancoa@gmail.com>
2024-05-31 08:56:22 -0500

    [Contact] - The Submit Contact Form Service

--- view/frontend/web/js/action/submit-contact-form.js
+++ view/frontend/web/js/action/submit-contact-form.js
@@ -3,11 +3,18 @@
  */
 
 define([
-    'ko'
-], function (ko) {
+    'ko',
+    'Barranco_Contact/js/model/url-builder',
+    'Barranco_Contact/js/model/submit-contact-form'
+], function (ko, urlBuilder, submitContactFormService) {
     'use strict';
 
     return function (contactFormData) {
-        return true;
+        let serviceUrl = urlBuilder.createUrl('/contact/us/submit-form', {}),
+            payload = {
+                contactInfo: contactFormData
+            };
+
+        return submitContactFormService(serviceUrl, payload);
     }
 });
---
+++ view/frontend/web/js/model/submit-contact-form.js
@@ -0,0 +1,13 @@
+/**
+ * Barranco Contact
+ */
+
+define([
+    'mage/storage'
+], function (storage) {
+    'use strict';
+
+    return function (serviceUrl, payload) {
+        return true;
+    }
+});
