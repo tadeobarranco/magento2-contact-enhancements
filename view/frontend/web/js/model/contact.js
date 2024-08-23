670ff4f0b2b08f69f7237577f0cdea659064b1e7
Tadeo Barranco <tbarrancoa@gmail.com>
2024-08-23 08:40:05 -0500

    [Contact] - The Contact Model

---
+++ view/frontend/web/js/model/contact.js
@@ -0,0 +1,16 @@
+/**
+ * Barranco Contact
+ */
+
+define([
+    'ko',
+    'domReady!'
+], function (ko) {
+    'use strict';
+
+    let reason = ko.observable(null);
+
+    return{
+        reason: reason
+    }
+})
