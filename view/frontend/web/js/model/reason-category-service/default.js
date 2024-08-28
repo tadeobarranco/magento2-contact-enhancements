6cf172c3532d3bef335f5dd79d8a4d86156122a9
Tadeo Barranco <tbarrancoa@gmail.com>
2024-08-28 08:03:46 -0500

    [Contact] - The Default Reason Category Service

---
+++ view/frontend/web/js/model/reason-category-service/default.js
@@ -0,0 +1,29 @@
+/**
+ * Barranco Contact
+ */
+
+define([
+    'Barranco_Contact/js/model/reason-service'
+], function (reasonService) {
+    'use strict';
+
+    return {
+
+        /**
+         * Get reason data for specific category
+         *
+         * @param  {String} category
+         */
+        getReason: function (category) {
+            reasonService.isLoading(true);
+
+            if (category !== undefined) {
+                reasonService.reason(category);
+            }
+
+            setTimeout(function () {
+                reasonService.isLoading(false);
+            }, 2000);
+        }
+    }
+});
