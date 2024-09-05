87bfb55de830aab97624b950ff66f612f0f227c0
Tadeo Barranco <tbarrancoa@gmail.com>
2024-09-05 08:16:31 -0500

    [Contact] - The Storage Get

--- view/frontend/web/js/model/reason-category-service/default.js
+++ view/frontend/web/js/model/reason-category-service/default.js
@@ -3,8 +3,11 @@
  */
 
 define([
-    'Barranco_Contact/js/model/reason-service'
-], function (reasonService) {
+    'Barranco_Contact/js/model/error-processor',
+    'Barranco_Contact/js/model/reason-service',
+    'Barranco_Contact/js/model/url-builder',
+    'mage/storage'
+], function (errorProcessor, reasonService, urlBuilder, storage) {
     'use strict';
 
     return {
@@ -15,16 +18,34 @@ define([
          * @param  {String} category
          */
         getReason: function (category) {
+            let serviceUrl = urlBuilder.createUrl('/contact/us/reason-information/:category', {
+                category: category
+            });
+
             reasonService.isLoading(true);
             reasonService.reason(false);
 
             if (category !== undefined) {
-                reasonService.reason(category);
+                return storage.get(
+                    serviceUrl, false
+                ).done(
+                    function (response) {
+                        reasonService.reason(response.reason);
+                    }
+                ).fail(
+                    function (response) {
+                        errorProcessor.process(response)
+                    }
+                ).always(
+                    function () {
+                        reasonService.isLoading(false);
+                    }
+                );
+            } else {
+                setTimeout(function () {
+                    reasonService.isLoading(false);
+                }, 2000);
             }
-
-            setTimeout(function () {
-                reasonService.isLoading(false);
-            }, 2000);
         }
     }
 });
