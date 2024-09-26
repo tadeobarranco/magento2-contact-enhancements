a931f18085df0ad3043bef431e666f50352fc649
Tadeo Barranco <tbarrancoa@gmail.com>
2024-09-26 08:07:14 -0500

    [Contact] - Extend The QuickSearch Widget: _onPropertyChange

--- view/frontend/web/js/search.js
+++ view/frontend/web/js/search.js
@@ -3,13 +3,96 @@
  */
 
 define([
-    'jquery'
-], function ($) {
+    'jquery',
+    'mage/template'
+], function ($, mageTemplate) {
     'use strict';
 
     $.widget('contact.search', $.mage.quickSearch, {
         _onPropertyChange: function () {
-            this._super();
+            let self = this,
+                searchField = this.element,
+                clonePosition = {
+                    position: 'absolute',
+                    // Removed to fix display issues
+                    // left: searchField.offset().left,
+                    // top: searchField.offset().top + searchField.outerHeight(),
+                    width: searchField.outerWidth()
+                },
+                source = this.options.template,
+                template = mageTemplate(source),
+                dropdown = $('<ul role="listbox"></ul>'),
+                name = this.element.attr('name'),
+                value = this.element.val();
+
+            this.submitBtn.disabled = true;
+
+            if (value.length >= parseInt(this.options.minSearchLength, 10)) {
+                this.submitBtn.disabled = false;
+
+                if (this.options.url !== '') { //eslint-disable-line eqeqeq
+                    $.getJSON(this.options.url, {
+                        reason: name,
+                        q: value
+                    }, $.proxy(function (data) {
+                        if (data.length) {
+                            $.each(data, function (index, element) {
+                                var html;
+
+                                element.index = index;
+                                html = template({
+                                    data: element
+                                });
+                                dropdown.append(html);
+                            });
+
+                            this._resetResponseList(true);
+
+                            this.responseList.indexList = this.autoComplete.html(dropdown)
+                                .css(clonePosition)
+                                .show()
+                                .find(this.options.responseFieldElements + ':visible');
+
+                            this.element.removeAttr('aria-activedescendant');
+
+                            if (this.responseList.indexList.length) {
+                                this._updateAriaHasPopup(true);
+                            } else {
+                                this._updateAriaHasPopup(false);
+                            }
+
+                            this.responseList.indexList
+                                .on('click', function (e) {
+                                    this.responseList.selected = $(e.currentTarget);
+                                    this.searchForm.trigger('submit');
+                                }.bind(this))
+                                .on('mouseenter mouseleave', function (e) {
+                                    this.responseList.indexList.removeClass(this.options.selectClass);
+                                    $(e.target).addClass(this.options.selectClass);
+                                    this.responseList.selected = $(e.target);
+                                    this.element.attr('aria-activedescendant', $(e.target).attr('id'));
+                                }.bind(this))
+                                .on('mouseout', function (e) {
+                                    if (!this._getLastElement() &&
+                                        this._getLastElement().hasClass(this.options.selectClass)) {
+                                        $(e.target).removeClass(this.options.selectClass);
+                                        this._resetResponseList(false);
+                                    }
+                                }.bind(this));
+                        } else {
+                            this._resetResponseList(true);
+                            this.autoComplete.hide();
+                            this._updateAriaHasPopup(false);
+                            this.element.removeAttr('aria-activedescendant');
+                        }
+                    }, this));
+                }
+            } else {
+                this._resetResponseList(true);
+                this.autoComplete.hide();
+                this._updateAriaHasPopup(false);
+                this.element.removeAttr('aria-activedescendant');
+            }
         }
     });
 
