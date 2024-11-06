96c3c39f0834eb1c3d5aafbee5227a7121a72a53
Tadeo Barranco <tbarrancoa@gmail.com>
2024-11-06 06:31:18 -0600

    [Contact] - Insert New Field To The Reason Form

--- Block/Reason/LayoutProcessor.php
+++ Block/Reason/LayoutProcessor.php
@@ -9,6 +9,35 @@ class LayoutProcessor implements LayoutProcessorInterface
      */
     public function process(array $jsLayout): array
     {
+        if (isset(
+            $jsLayout['components']['contact']['children']['steps']['children']
+            ['reason-step']['children']['contact-reason-fieldset']['children']
+        )) {
+            $jsLayout['components']['contact']['children']['steps']['children']
+            ['reason-step']['children']['contact-reason-fieldset']['children']
+            ['form-fields'] = [
+                'component' => 'uiComponent',
+                'displayArea' => 'additional-form-fields',
+                'children' => [
+                    'order_number' => [
+                        'component' => 'Magento_Ui/js/form/element/abstract',
+                        'config' => [
+                            'customScope' => 'contactReasonForm',
+                            'template' => 'ui/form/field',
+                            'elementTmpl' => 'ui/form/element/input',
+                        ],
+                        'dataScope' => 'contactReasonForm.order_number',
+                        'label' => __('Order Number'),
+                        'provider' => 'contactProvider',
+                        'sortOrder' => 2,
+                        'validation' => [
+                            'required-entry' => false,
+                        ]
+                    ]
+                ]
+            ];
+        }
+
         return $jsLayout;
     }
 }
