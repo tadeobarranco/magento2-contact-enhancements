eb71d22d8edc5b47d8563926968b372cf4eea0ec
Tadeo Barranco <tbarrancoa@gmail.com>
2024-11-15 06:49:13 -0600

    [Contact] - Insert Reason Fields By Tha LayoutProcessor

--- Block/Reason/LayoutProcessor.php
+++ Block/Reason/LayoutProcessor.php
@@ -11,33 +11,101 @@ class LayoutProcessor implements LayoutProcessorInterface
     {
         if (isset(
             $jsLayout['components']['contact']['children']['steps']['children']
-            ['reason-step']['children']['contact-reason-fieldset']['children']
+            ['reason-step']['children']
         )) {
             $jsLayout['components']['contact']['children']['steps']['children']
-            ['reason-step']['children']['contact-reason-fieldset']['children']
-            ['form-fields'] = [
+            ['reason-step']['children']['reason-form-fields'] = [
                 'component' => 'uiComponent',
-                'displayArea' => 'additional-form-fields',
-                'children' => [
-                    'order_number' => [
-                        'component' => 'Magento_Ui/js/form/element/abstract',
-                        'config' => [
-                            'customScope' => 'contactReasonForm',
-                            'template' => 'ui/form/field',
-                            'elementTmpl' => 'ui/form/element/input',
-                        ],
-                        'dataScope' => 'contactReasonForm.order_number',
-                        'label' => __('Order Number'),
-                        'provider' => 'contactProvider',
-                        'sortOrder' => 2,
-                        'validation' => [
-                            'required-entry' => false,
-                        ]
-                    ]
-                ]
+                'displayArea' => 'additional-reason-form-fields',
+                'children' => $this->getReasonFields()
             ];
         }
 
         return $jsLayout;
+    }
+
+    /**
+     * Populate fields based on the contact reason
+     *
+     * @return array
+     */
+    private function getReasonFields(): array
+    {
+        $fields = [];
+        $baseFieldComponent = $this->getBaseFieldComponent();
+        $reasonsFields = array_merge(
+            $this->getOrdersFields(),
+            $this->getPromotionsFields(),
+            $this->getProductsFields(),
+        );
+        $sortOrder = 2;
+
+        foreach ($reasonsFields as $field => $label) {
+            $fields[$field] = array_merge($baseFieldComponent, [
+                'dataScope' => "contactReason.$field",
+                'label' => $label,
+                'sortOrder' => $sortOrder
+            ]);
+            $sortOrder++;
+        }
+
+        return $fields;
+    }
+
+    /**
+     * Get base field component configurations
+     *
+     * @return array
+     */
+    private function getBaseFieldComponent(): array
+    {
+        return [
+            'component' => 'Magento_Ui/js/form/element/abstract',
+            'config' => [
+                'customScope' => 'contactReasonForm',
+                'template' => 'ui/form/field',
+                'elementTmpl' => 'ui/form/element/input',
+            ],
+            'provider' => 'contactProvider',
+            'validation' => [
+                'required-entry' => true,
+            ]
+        ];
+    }
+
+    /**
+     * Get predefined fields for the orders contact reason
+     *
+     * @return array
+     */
+    private function getOrdersFields(): array
+    {
+        return [
+            'order_id' => __('Order ID'),
+        ];
+    }
+
+    /**
+     * Get predefined fields for the promotions contact reason
+     *
+     * @return array
+     */
+    private function getPromotionsFields(): array
+    {
+        return [
+            'promotion_code' => __('Promotion Code'),
+        ];
+    }
+
+    /**
+     * Get predefined fields for the products contact reason
+     *
+     * @return array
+     */
+    private function getProductsFields(): array
+    {
+        return [
+            'product_sku' => __('Product SKU'),
+        ];
     }
 }
